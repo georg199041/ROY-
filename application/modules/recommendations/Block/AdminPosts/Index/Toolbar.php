3cf7fddd20a9aa323607ea47deede482a01d78a0
Павленко Евгений <pavlenko.obs@gmail.com>
2012-11-15 17:12:41 +0200

    layout fix

--- application/modules/recommendations/Block/AdminPosts/Index/Toolbar.php
+++ application/modules/recommendations/Block/AdminPosts/Index/Toolbar.php
@@ -8,37 +8,37 @@ class Recommendations_Block_AdminPosts_Index_Toolbar extends Core_Block_Toolbar_
 		
 		$this->addButton(array(
 			'name'       => 'show',
-			'title'      => $this->__('Show'),
+			'title'      => $this->__('Показать'),
 			'urlOptions' => '*/*/enabled/value/YES'
 		));
 		
 		$this->addButton(array(
 			'name'       => 'hide',
-			'title'      => $this->__('Hide'),
+			'title'      => $this->__('Скрыть'),
 			'urlOptions' => '*/*/enabled/value/NO'
 		));
 
 		$this->addButton(array(
 			'name'       => 'move',
-			'title'      => $this->__('Move'),
+			'title'      => $this->__('Переместить'),
 			'urlOptions' => '*/*/move'
 		));
 		
 		$this->addButton(array(
 			'name'       => 'copy',
-			'title'      => $this->__('Copy'),
+			'title'      => $this->__('Копировать'),
 			'urlOptions' => '*/*/copy'
 		));
 		
 		$this->addButton(array(
 			'name'       => 'delete',
-			'title'      => $this->__('Delete'),
+			'title'      => $this->__('Удалить'),
 			'urlOptions' => '*/*/delete'
 		));
 		
 		$this->addButton(array(
 			'name'       => 'add',
-			'title'      => $this->__('Add'),
+			'title'      => $this->__('Создать'),
 			'urlOptions' => '*/*/edit'
 		));
 	}
