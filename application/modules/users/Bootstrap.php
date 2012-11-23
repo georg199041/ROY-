7a67ba25bff9d0e725814651fa791f9cfd7ed749
Павленко Евгений <pavlenko.obs@gmail.com>
2012-11-23 15:19:46 +0200

    Add auth plugin

--- application/modules/users/Bootstrap.php
+++ application/modules/users/Bootstrap.php
@@ -4,5 +4,10 @@ require_once 'Core/Application/Module/Bootstrap.php';
 
 class Users_Bootstrap extends Core_Application_Module_Bootstrap
 {    
-    
+	public function _initPlugins()
+	{
+		$this->bootstrap('frontController');
+		$front = $this->getResource('frontController');
+		//$front->registerPlugin(new Users_Controller_Plugin_Auth(array('route' => 'users/admin-users/login')));
+	}
 }
